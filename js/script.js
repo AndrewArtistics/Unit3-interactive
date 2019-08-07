239735a6622428ac16cbce314ff345443d2feda1
AndrewArtistics <50923847+AndrewArtistics@users.noreply.github.com>
2019-08-06 20:57:59 -0700

    Added function to show 'other' input when selected
    input element for 'other' option is initially hidden. Once selected the input is then shown and re-hidden if it's deselected afterwards.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@
 //Sets the focus on the name input field
 $("#name").focus();
 
+//initially hides "other-job" input element
+$('#other-title').hide();
+    //shows "other-job" input when 'other' option is selected (animations for smoother looking transitions/extra flare)
+$('#title').change(function(){
+    let jobRoles = $('#title').val();
+    if (jobRoles == 'other'){
+        $('#other-title').fadeIn(500); //input quickly fades in when other is selected.
+    } else {
+        $('#other-title').slideUp(500); //slides up and hides input if other deselected!
+    };
+});
+
+//stuff shh
