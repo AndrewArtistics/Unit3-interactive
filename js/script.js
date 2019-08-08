0a6b08527e90bdf610c5614b00e96ba2537e4f1d
AndrewArtistics <50923847+AndrewArtistics@users.noreply.github.com>
2019-08-07 23:24:23 -0700

    Shirt selection
    made global selections for shirt options to hide on load
    function to hide/show color selections for associated theme selection

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ $("#name").focus();
 
 $('#other-title').hide(); //initially hides "other-job" input element
 
+//hides the 'select theme' after a theme is choses and hides the color options until a theme is chosen.
+$('#design').children('option:eq(0)').hide();
+$('#color').prepend('<option disabled selected value="">Please select a T-shirt theme.</option>');
+$('#color').children().hide();
+
 $('#credit-card').hide(); //hides credit card inputs on load
 //finds and appends ID's to the paypal and bitcoin divs and then hides them on load
 $("#credit-card").next().attr('id', 'paypal');
@@ -35,14 +40,39 @@ $('#title').change(function(){
     };
 });
 
-//stuff shh
-// $('#color').hide();
+/***************
+T-shirt and Color selection
+***************/
+
+//shows appropriate color selection for each theme once one is selected.
+$('#design').change(function(){
+    let design = $('#design').val();
+
+    if (design == 'js puns'){
+        $('#color').children().hide();
+        $('#color').children('option:eq(1)').show();
+        $('#color').children('option:eq(1)').next().show();
+        $('#color').children('option:eq(1)').next().next().show();
+        $('#color').val('cornflowerblue');
+    };
+    if (design == 'heart js'){
+        $('#color').children().hide();
+        $('#color').children('option:eq(4)').show();
+        $('#color').children('option:eq(4)').next().show();
+        $('#color').children('option:eq(4)').next().next().show();
+        $('#color').val('tomato');
+    };
+});
+
+/*************************
+Activities registration
+*************************/
 
 
 
 /***************
 Payment methods
- ***************/
+***************/
 $('#payment').change(function(){
     let payMethod = $('#payment').val();
 
@@ -67,3 +97,10 @@ $('#payment').change(function(){
         $('#bitcoin').fadeIn(500);
     };
 });
+
+/*************************
+Regex form validations
+Displays proper error messages for blank/incomplete forms
+regex to check for valid input/formats for name, email and credit information
+*************************/
+
